10a795a6877c78fb50bbd7732d6607ddb0a2f750
Massimiliano Angelino <angmas@amazon.com>
2020-02-18 14:34:00 +0100

    Correct handling of incorrectly paired devices

--- Example/AmazonFreeRTOSDemo/AmazonFreeRTOSDemo/DevicesViewController.swift
+++ Example/AmazonFreeRTOSDemo/AmazonFreeRTOSDemo/DevicesViewController.swift
@@ -17,7 +17,6 @@ class DevicesViewController: UITableViewController {
 
         // Add observe for AmazonFreeRTOSManager NSNotifications
         NotificationCenter.default.addObserver(self, selector: #selector(centralManagerDidUpdateState), name: .afrCentralManagerDidUpdateState, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(peripheralDidDiscoverCharacteristics), name: .afrPeripheralDidDiscoverCharacteristics, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(centralManagerDidDisconnectDevice), name: .afrCentralManagerDidDisconnectDevice, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(reloadDataWithoutAnimation), name: .afrCentralManagerDidDiscoverDevice, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(reloadDataWithoutAnimation), name: .afrCentralManagerDidConnectDevice, object: nil)
@@ -58,23 +57,6 @@ extension DevicesViewController {
             return
         }
         AmazonFreeRTOSManager.shared.stopScanForDevices()
-    }
-
-    @objc
-    func peripheralDidDiscoverCharacteristics(_ notification: NSNotification) {
-        if let userInfo = notification.userInfo as? [String: Any] {
-            if let peripheral = userInfo["peripheral"] as? CBPeripheral {
-                guard AmazonFreeRTOSManager.shared.devices[peripheral.identifier]?.mtu != nil else {
-                    DispatchQueue.main.async {
-                        Alertift.alert(title: NSLocalizedString("Error", comment: String()), message: "Unable to deocode data from device. Forget the device and pair it again")
-                            .action(.default(NSLocalizedString("OK", comment: String())))
-                            .show(on: self)
-                    }
-                    AmazonFreeRTOSManager.shared.devices[peripheral.identifier]?.disconnect()
-                    return
-                }
-            }
-        }
     }
 
     @objc
@@ -223,31 +205,40 @@ extension DevicesViewController {
         uuid = Array(AmazonFreeRTOSManager.shared.devices.keys)[indexPath.row]
         let device = Array(AmazonFreeRTOSManager.shared.devices.values)[indexPath.row]
         if device.peripheral.state == .connected {
-            Alertift.actionSheet()
-                .popover(anchorView: cell)
+            if device.mtu != nil {
+                Alertift.actionSheet()
+                    .popover(anchorView: cell)
 
-                // Example 1: MQTT Proxy
+                    // Example 1: MQTT Proxy
 
-                .action(.default(NSLocalizedString("MQTT Proxy", comment: String()))) { _, _ in
-                    self.performSegue(withIdentifier: "toMqttProxyViewController", sender: self)
-                    return
+                    .action(.default(NSLocalizedString("MQTT Proxy", comment: String()))) { _, _ in
+                        self.performSegue(withIdentifier: "toMqttProxyViewController", sender: self)
+                        return
+                    }
+
+                    // Example 2: Network Config
+
+                    .action(.default(NSLocalizedString("Network Config", comment: String()))) { _, _ in
+                        self.performSegue(withIdentifier: "toNetworkConfigViewController", sender: self)
+                        return
+                    }
+
+                    // Example 3: Custom GATT MQTT
+
+                    .action(.default(NSLocalizedString("Custom GATT MQTT", comment: String()))) { _, _ in
+                        self.performSegue(withIdentifier: "toCustomGattMqttViewController", sender: self)
+                        return
+                    }
+                    .action(.cancel(NSLocalizedString("Cancel", comment: String())))
+                    .show(on: self)
+            } else {
+                if let deviceCell = cell as? DeviceCell {
+                    deviceCell.viewDeviceStateIndicator.backgroundColor = UIColor(named: "orange_color")
+                    Alertift.alert(title: NSLocalizedString("Error", comment: String()), message: "Unable to read secure values. You need to forget this device pair it again")
+                        .action(.default(NSLocalizedString("OK", comment: String())))
+                        .show(on: self)
                 }
-
-                // Example 2: Network Config
-
-                .action(.default(NSLocalizedString("Network Config", comment: String()))) { _, _ in
-                    self.performSegue(withIdentifier: "toNetworkConfigViewController", sender: self)
-                    return
-                }
-
-                // Example 3: Custom GATT MQTT
-
-                .action(.default(NSLocalizedString("Custom GATT MQTT", comment: String()))) { _, _ in
-                    self.performSegue(withIdentifier: "toCustomGattMqttViewController", sender: self)
-                    return
-                }
-                .action(.cancel(NSLocalizedString("Cancel", comment: String())))
-                .show(on: self)
+            }
         } else {
             device.connect(reconnect: true, credentialsProvider: AWSMobileClient.default())
         }
