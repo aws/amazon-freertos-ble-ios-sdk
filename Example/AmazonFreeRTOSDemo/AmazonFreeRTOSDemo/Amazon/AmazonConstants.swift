972384b65c0b056c9177521792d5228610f45389
Carl Lundin <lundinc@amazon.com>
2020-07-17 15:21:28 -0700

    Revert "Change compiler errors to warnigns for CI"
    This reverts commit e4f18255318eba3f5b10e83a11a02b50b0cf1cc3.

--- Example/AmazonFreeRTOSDemo/AmazonFreeRTOSDemo/Amazon/AmazonConstants.swift
+++ Example/AmazonFreeRTOSDemo/AmazonFreeRTOSDemo/Amazon/AmazonConstants.swift
@@ -6,15 +6,15 @@ struct AmazonConstants {
 
         #warning("if you do not plan to use the MQTT demo, you can remove these #error.")
 
-        #warning("Replace with your AWS Region. eg: AWSRegionType.USEast1")
+        #error("Replace with your AWS Region. eg: AWSRegionType.USEast1")
 
         static let region = AWSRegionType.USEast1
 
-        #warning("Replace with your AWS IoT Policy Name. eg: MyIoTPolicy")
+        #error("Replace with your AWS IoT Policy Name. eg: MyIoTPolicy")
 
         static let iotPolicyName = "MyIoTPolicy"
 
-        #warning("Also update FreeRTOSDemo/Support/awsconfiguration.json with your credentials.")
+        #error("Also update FreeRTOSDemo/Support/awsconfiguration.json with your credentials.")
 
         static let mqttCustomTopic = "my/custom/topic"
     }
